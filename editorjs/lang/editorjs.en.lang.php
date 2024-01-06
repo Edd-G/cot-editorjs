19286590038f2c0ac9e82e213897398171bd0ff2
Edd-G <ed.gabishev@gmail.com>
2024-01-06 11:21:16 +0300

    Added empty values for English language

--- editorjs/lang/editorjs.en.lang.php
+++ editorjs/lang/editorjs.en.lang.php
@@ -12,3 +12,66 @@ defined('COT_CODE') or die('Wrong URL');
 
 $L['cfg_prefix'] = 'Prefix of CSS classes added by parser';
 $L['cfg_prefix_hint'] = 'Default: prs';
+
+// Translate UI
+$L['i18n_direction'] = 'ltr';
+
+// UI
+$L['i18n_ui_blocktunes_toggler_clicktotune'] = '';
+$L['i18n_ui_blocktunes_toggler_ordragtomove'] = '';
+$L['i18n_ui_inlinetoolbar_converter_convertto'] = '';
+$L['i18n_ui_toolbar_toolbox_add'] = '';
+$L['i18n_ui_popover_filter'] = '';
+$L['i18n_ui_popover_nothingfound'] = '';
+
+// Tool names
+$L['i18n_toolnames_text'] = '';
+$L['i18n_toolnames_heading'] = '';
+$L['i18n_toolnames_list'] = '';
+$L['i18n_toolnames_warning'] = '';
+$L['i18n_toolnames_checklist'] = '';
+$L['i18n_toolnames_quote'] = '';
+$L['i18n_toolnames_code'] = '';
+$L['i18n_toolnames_delimiter'] = '';
+$L['i18n_toolnames_raw_html'] = '';
+$L['i18n_toolnames_table'] = '';
+$L['i18n_toolnames_link'] = '';
+$L['i18n_toolnames_marker'] = '';
+$L['i18n_toolnames_bold'] = '';
+$L['i18n_toolnames_italic'] = '';
+$L['i18n_toolnames_underline'] = '';
+$L['i18n_toolnames_inlinecode'] = '';
+$L['i18n_toolnames_alert'] = '';
+$L['i18n_toolnames_image'] = '';
+
+// Tools
+$L['i18n_tools_link_addalink'] = '';
+$L['i18n_tools_stub_blockcannotdisplayed'] = '';
+$L['i18n_tools_image_caption'] = '';
+$L['i18n_tools_image_selectanimage'] = '';
+$L['i18n_tools_image_withborder'] = '';
+$L['i18n_tools_image_stretchimage'] = '';
+$L['i18n_tools_image_withbackground'] = '';
+$L['i18n_tools_code'] = '';
+$L['i18n_tools_linktool_link'] = '';
+$L['i18n_tools_linktool_couldntfetch'] = '';
+$L['i18n_tools_linktool_couldntgetdata'] = '';
+$L['i18n_tools_linktool_wrongresponse'] = '';
+$L['i18n_tools_header_header'] = '';
+$L['i18n_tools_paragraph_entersomething'] = '';
+$L['i18n_tools_list_ordered'] = '';
+$L['i18n_tools_list_unordered'] = '';
+
+// Block tunes
+$L['i18n_blocktunes_delete'] = '';
+$L['i18n_blocktunes_clicktodelete'] = '';
+$L['i18n_blocktunes_moveup'] = '';
+$L['i18n_blocktunes_movedown'] = '';
+
+// Editor config
+$L['tools_warning_title'] = '';
+$L['tools_warning_message'] = '';
+$L['tools_quote_quote'] = '';
+$L['tools_quote_caption'] = '';
+
+$L['editor_placeholder'] = '';
